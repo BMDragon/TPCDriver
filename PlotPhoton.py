efb78a28bf6ac9bcf659331cacaa919b040e36fe
Brandon Weiss <brandon.weiss@duke.edu>
2022-07-08 14:31:37 -0700

    Touch up the photon path plots

--- PlotPhoton.py
+++ PlotPhoton.py
@@ -1,14 +1,13 @@
 # Created by Brandon Weiss on 7/8/2022
-from matplotlib import projections
 import numpy as np
 import matplotlib.pyplot as plt
 
 record = np.load('./Data/records.npy', allow_pickle='TRUE').item()
 
-photon = 25
-xPos = np.array([])
-yPos = np.array([])
-zPos = np.array([])
+photon = 69420
+xPos = np.array([0])
+yPos = np.array([0])
+zPos = np.array([0.025])
 
 n = 1
 while photon in record['step'+str(n)]['status']['photon'][0]:
@@ -20,7 +19,11 @@ while photon in record['step'+str(n)]['status']['photon'][0]:
 
 fig = plt.figure(1)
 ax = plt.axes(projection='3d')
-ax.scatter3D(xPos, yPos, zPos)
+ax.plot(xPos, yPos, zPos, marker='.', ls='-', color='b', markerfacecolor='r', markeredgecolor='r')
+ax.set_xlim(-0.025,0.025)
+ax.set_ylim(-0.025,0.025)
+ax.set_zlim(0,0.05)
+ax.set_xlabel('X')
 for i in range(len(xPos)):
-    ax.text(xPos[i], yPos[i], zPos[i], str(i+1))
+    ax.text(xPos[i], yPos[i], zPos[i], str(i))
 plt.show()
